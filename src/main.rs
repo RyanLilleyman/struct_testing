b85d445274893b8db976937b66d6d6545008a0d6
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-15 11:03:55 -0400

    Add ..instance syntax to show how to create a new instance of a structure from an existing structure in this case user2

--- src/main.rs
+++ src/main.rs
@@ -31,4 +31,20 @@ fn main() {
     );
     println!("{}", name);
     println!("{:?}", user2);
+
+    // creating new instances of a struct fron an existing instance
+    let user3: User = User {
+        email: String::from("newemail@gmail.com"),
+        username: String::from("hello"),
+        ..user2
+    };
+
+    println!("{:?}", user3);
+
+    //tuple struct
+    //notice neither have named fields just expected types
+    //the entire struct name bundles these types
+    //together and expects relevant and valid data
+    //struct Color(i32, i32, i32);
+    //struct Point(i32, i32, i32);
 }
